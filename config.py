902693ac7828447503f92cd84b96d2cb2f5ed45e
Ajinkyakulkarni314 <124431835+Ajinkyakulkarni314@users.noreply.github.com>
2023-02-04 17:32:18 +0530

    Adding Project Files

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+CSV_FILE_PATH = "Enter_path"
+PORT_NUMBER  = 5005
+
+Model_file_path = os.path.join("project_app", "iris.pkl")
+
+Json_file_path = os.path.join("project_app", "project_data.json")
